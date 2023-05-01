832f3ab0d6c2818d37f77a5c2f9cd72ecdc48db1
Abhishek Sawalkar <abhishek31sawalkar@gmail.com>
2023-05-01 14:54:58 -0400

    Final project done

--- ARK/app.py
+++ ARK/app.py
@@ -1,120 +1,133 @@
-# import jsonify as jsify
 import json
-from flask import Flask,render_template,jsonify
+from flask import Flask, request, render_template, jsonify
 import psycopg2
 import os
 from dotenv import load_dotenv
-import datetime as dt
-load_dotenv("local.env") 
-# load_dotenv() 
-app=Flask(__name__)
-
-# @app.route('/')
-# def hello():
-#     return "hello world"
+from datetime import datetime
+#Getting env variables
+load_dotenv("local.env")
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    connect=psycopg2.connect(
+    # Connect to backed postgresql on page load
+    connect = psycopg2.connect(
         database=os.getenv('POSTGRES_DB'),
         user=os.getenv('POSTGRES_USER'),
         password=os.getenv('POSTGRES_PASSWORD'),
         host=os.getenv('POSTGRES_HOST'),
         port=os.getenv('POSTGRES_PORT')
     )
-    cursor=connect.cursor()
-    query='SELECT * FROM "CM_HAM_DO_AI1/Temp_value"'
+    cursor = connect.cursor()
+    query = 'SELECT * FROM "CM_HAM_DO_AI1/Temp_value"'
     cursor.execute(query)
-    query_data=cursor.fetchall()
-    # print("hello")
-    # print(type(data))
-    # print(data)
-    x_values=[]
-    y_values=[]
-    for x,y in query_data:
-        x=x.strftime('%m/%d/%Y %H:%M:%S:%f')
-        # print(x) 
-        # print(type(x))
-        x_values.append(x)
-        y_values.append(y) 
-        # print(y)
-        # print(type(y))
-    # cursor.commit()
+    #Retrieving all initial rows of temp_value table
+    query_data = cursor.fetchall()
 
-    # print(x_values)
-    # print(y_values)
-    
+    x_values = []
+    y_values = []
+    for x, y in query_data:
+        #Converting datetime to string
+        x = x.strftime('%m/%d/%Y %H:%M:%S:%f')
+        x_values.append(x)
+        y_values.append(y)
+
     cursor.close()
     connect.close()
-    return render_template('dashboard.html',x_values=x_values,y_values=y_values)
+    # Returning x and y values to dashboard
+    return render_template('dashboard.html', x_values=x_values, y_values=y_values)
 
-@app.route('/graph/<graph>')
-def get_graph_data(graph):
+
+@app.route('/graph/', methods=['POST'])
+def get_graph_data():
   # get the data for the specified graph
-  # this could be a function call that returns the data for the graph
-    connect=psycopg2.connect(
+    graph = request.json.get('graph')
+    start = request.json.get('start')
+    end = request.json.get('end')
+    
+    connect = psycopg2.connect(
         database=os.getenv('POSTGRES_DB'),
         user=os.getenv('POSTGRES_USER'),
         password=os.getenv('POSTGRES_PASSWORD'),
         host=os.getenv('POSTGRES_HOST'),
         port=os.getenv('POSTGRES_PORT')
     )
-    cursor=connect.cursor()
-    print(graph)
-    if graph=="1":
-        query='SELECT * FROM "CM_HAM_DO_AI1/Temp_value"'
-        name="Temperature"
-        unit="Celsius"
-    elif graph=="2":
-        query='SELECT * FROM "CM_HAM_PH_AI1/pH_value"'
-        name="pH Value"
-        unit=""
+    cursor = connect.cursor()
+    if graph == 1:
+        if start == 0 and end == 0:
+            #When we just need to load the whole graph
+            query = 'SELECT * FROM "CM_HAM_DO_AI1/Temp_value"'
+        else:
+            #When we need to retrieve specific time period
+            date_format = "%m/%d/%Y %H:%M:%S:%f"
+            start = datetime.strptime(start, date_format)
+            end = datetime.strptime(end, date_format)
+            query = f"SELECT * FROM \"CM_HAM_DO_AI1/Temp_value\" WHERE time >= '{start}' AND time <= '{end}'"
+        name = "Temperature"
+        unit = "Celsius"
+    elif graph == 2:
+        if start == 0 and end == 0:
+            query = 'SELECT * FROM "CM_HAM_PH_AI1/pH_value"'
+        else:
+            date_format = "%m/%d/%Y %H:%M:%S:%f"
+            start = datetime.strptime(start, date_format)
+            end = datetime.strptime(end, date_format)
+            query = f"SELECT * FROM \"CM_HAM_PH_AI1/pH_value\" WHERE time >= '{start}' AND time <= '{end}'"
+        name = "pH Value"
+        unit = ""
 
-    elif graph=="3":
-        query='SELECT * FROM "CM_PID_DO/Process_DO"'
-        name="Process Dissolved Oxygen"
-        unit="%"
+    elif graph == 3:
+        if start == 0 and end == 0:
+            query = 'SELECT * FROM "CM_PID_DO/Process_DO"'
+        else:
+            date_format = "%m/%d/%Y %H:%M:%S:%f"
+            start = datetime.strptime(start, date_format)
+            end = datetime.strptime(end, date_format)
+            query = f"SELECT * FROM \"CM_PID_DO/Process_DO\" WHERE time >= '{start}' AND time <= '{end}'"
+        name = "Process Dissolved Oxygen"
+        unit = "%"
 
-    else: 
-        query='SELECT * FROM "CM_PRESSURE/Output"'
-        name="Pressure Output"
-        unit="psi"
-
+    else:
+        if start == 0 and end == 0:
+            query = 'SELECT * FROM "CM_PRESSURE/Output"'
+        else:
+            date_format = "%m/%d/%Y %H:%M:%S:%f"
+            start = datetime.strptime(start, date_format)
+            end = datetime.strptime(end, date_format)
+            query = f"SELECT * FROM \"CM_PRESSURE/Output\" WHERE time >= '{start}' AND time <= '{end}'"
+        name = "Pressure Output"
+        unit = "psi"
 
     cursor.execute(query)
-    query_data=cursor.fetchall()
-    
-    x_values=[]
-    y_values=[]
-    metadata=[]
+    query_data = cursor.fetchall()
+
+    x_values = []
+    y_values = []
+    metadata = []
     metadata.append(name)
     metadata.append(unit)
-    
-    for x,y in query_data:
-        x=x.strftime('%m/%d/%Y %H:%M:%S:%f')
+
+    for x, y in query_data:
+        x = x.strftime('%m/%d/%Y %H:%M:%S:%f')
         x_values.append(x)
-        y_values.append(y) 
-       
+        y_values.append(y)
+
     cursor.close()
     connect.close()
-    
-    #Converting x and y variables into a dictionary
-    data={'x_values':x_values,'y_values':y_values,'metadata':metadata}
+
+    # Converting x and y variables into a dictionary
+    data = {'x_values': x_values, 'y_values': y_values, 'metadata': metadata}
     # print(data)
-    jd=json.dumps(data) 
-    json_response=json.loads(jd)
-    # print(type(json_response))
-    # we convert the data dictionary to JSON and then back to a python dictionary so 
-    # that we can use the jsonify function to create a Flask response object with the 
+    jd = json.dumps(data)
+    json_response = json.loads(jd)
+    # we convert the data dictionary to JSON and then back to a python dictionary so
+    # that we can use the jsonify function to create a Flask response object with the
     # correct headers and formatting for returning JSON-formatted data.
     return jsonify(json_response)
 
-if __name__=="__main__":
-    host="0.0.0.0"
-    # host="localhost"
-    port=8888 
-    debug=True
-    app.run(host=host,port=8888,debug=True)
-    # app.run(debug=True)
-    # print(os.getenv('POSTGRES_HOST'))
-    # print(os.getenv('VAR_A'))
+
+if __name__ == "__main__":
+    host = "0.0.0.0"
+    port = 8888
+    debug = False
+    app.run(host=host, port=8888, debug=debug)
